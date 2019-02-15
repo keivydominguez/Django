6e6f0e26397bb9621e6f76068e8632b10181fb50
Keivy Sanchez <keivysanchez@gmail.com>
2019-02-15 13:46:50 -0600

    modifications of index,admin

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,7 @@ from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    content_type = 'latest_question_list'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
